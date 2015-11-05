424987290da93ebea7e16dbf6c2a94baf56a7f0f
Robert Bene <robert_bene@mapcat.net>
2015-11-05 13:21:49 +0100

    Add comments.

--- plot1.R
+++ plot1.R
@@ -1,18 +1,26 @@
 plot1 <- function(filename, startDate = "2007-02-01", endDate = "2007-02-02") {
+    ## Read full data
     data <- read.table(filename, header=TRUE, sep=";", na.strings = "?");
+    
+    ## Init start/end date
     start <- as.Date(startDate);
     end <- as.Date(endDate);
     
+    ## Get subset
     dataSubset <- subset(data, as.Date(Date, format = "%d/%m/%Y") >= start & as.Date(Date, format = "%d/%m/%Y") <= end);
+    
+    ## Remove the full data buffer
     rm(data);
     
-    title  <- "Global Active Power";
-    xLabel <- "Global Active Power (kilowatts)";
-    yLabel <- "Frequency";
-    
+    ## Fix margins
     par(mar = c(4,4,2,2));
     
-    hist(dataSubset$Global_active_power, col = "red", xlab = xLabel, ylab = yLabel, main = title);
-    dev.copy(png, file="plot1.png", width = 480, height = 480)
+    ## Create histogram
+    hist(dataSubset$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power");
+    
+    ## Copy to PNG file
+    dev.copy(png, file="plot1.png", width = 480, height = 480);
+    
+    ## Close PNG device
     dev.off();
 }
