382f04948db6f47a24b41eaf68bc5045c1a0d851
Robert Bene <robert_bene@mapcat.net>
2015-11-05 14:02:10 +0100

    Fixing labels.

--- plot3.R
+++ plot3.R
@@ -21,13 +21,13 @@ plot3 <- function(filename, startDate = "2007-02-01", endDate = "2007-02-02") {
     
     ## Create plot (type = line)
     with(dataSubset, {
-        plot(Sub_metering_1~datetimePos, type="l",  ylab="Global Active Power (kilowatts)", xlab="")
+        plot(Sub_metering_1~datetimePos, type="l",  ylab="Energy sub metering", xlab="")
         lines(Sub_metering_2~datetimePos,col='red')
         lines(Sub_metering_3~datetimePos,col='blue')
     });
     
     ## Add legend
-    legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"));
+    legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), pch="-");
     
     ## Copy to PNG file
     dev.copy(png, file="plot3.png", width = 480, height = 480);
